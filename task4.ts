c386aea7d5ebc38359972fc677952ee2ade5ae21
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 13:33:00 +0200

    Add fetch request for advice

--- task4.ts
+++ task4.ts
@@ -10,3 +10,19 @@ type AdviceData = {
 const fetchAdvice = async (): Promise<void> => {
   // this function will get advice from the API
 };
+
+
+
+type AdviceData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+const fetchAdvice = async (): Promise<void> => {
+  // this function gets advice from the API
+
+  const response = await fetch("https://api.adviceslip.com/advice");
+  // wait for the API response
+};

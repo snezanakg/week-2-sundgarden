3e89a323f9b3955f9a70b2183c858dd860b12061
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 15:41:51 +0200

    Start two fetches task

---
+++ task6.ts
@@ -0,0 +1,14 @@
+// Task 6 - Two Fetches in a Row
+
+type AdviceData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+const fetchAdvice = async (): Promise<string> => {
+  // this function will return one advice as a string
+
+  return "";
+};

fe31c7e4cef91e7433f36cfb5bda93e37350cc0f
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 13:21:21 +0200

    Add timeout to delayed message

--- task3.ts
+++ task3.ts
@@ -9,3 +9,18 @@ const delayedMessage = (
   return new Promise((resolve) => {
   });
 };
+
+
+
+const delayedMessage = (
+  message: string,
+  seconds: number
+): Promise<string> => {
+  // this function should return a Promise with a string
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // after waiting, I will resolve the Promise
+    }, seconds * 1000); // change seconds to milliseconds
+  });
+};

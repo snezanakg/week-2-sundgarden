5a83f3f429be1f5f6487f8ec355254e5bf6d2089
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 13:32:11 +0200

    Start fetching advice task

---
+++ task4.ts
@@ -0,0 +1,12 @@
+// Task 4 - Fetching Advice
+
+type AdviceData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+const fetchAdvice = async (): Promise<void> => {
+  // this function will get advice from the API
+};

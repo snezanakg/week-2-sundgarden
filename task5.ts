2464087f55e64cb0a6064474b9c619b024a91efc
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 15:32:58 +0200

    Start checking response task

---
+++ task5.ts
@@ -0,0 +1,12 @@
+// Task 5 - Checking the Response
+
+type AdviceData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+const fetchAdvice = async (): Promise<void> => {
+  // this function will fetch advice and check the response
+};

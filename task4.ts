c51639f805cdcbd7d392461278f8b7f5c26d5da4
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 13:52:35 +0200

    Fix advice data scope

--- task4.ts
+++ task4.ts
@@ -11,16 +11,16 @@ type AdviceData = {
 const fetchAdvice = async (): Promise<void> => {
   // this function gets advice from the API
 
-  const response = await fetch("https://api.adviceslip.com/advice");
+
+
+    const response = await fetch("https://api.adviceslip.com/advice");
   // wait for the API response
 
   const data: AdviceData = await response.json();
   // change the response into JSON and save it as AdviceData
-};
 
- console.log(data.slip.advice);
+  console.log(data.slip.advice);
   // show only the advice text
 };
-
 
 fetchAdvice(); // start the async function

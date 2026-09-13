7e1b17438f18f2e4da9d094c2b0f4005142ac4dd
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-13 13:20:22 +0200

    Start delayed message task

---
+++ task3.ts
@@ -0,0 +1,11 @@
+// Task 3 - Delayed Message
+
+const delayedMessage = (
+  message: string,
+  seconds: number
+): Promise<string> => {
+  // this function should return a Promise with a string
+
+  return new Promise((resolve) => {
+  });
+};

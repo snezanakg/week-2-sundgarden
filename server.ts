2c2e0ec2451c686edba9a52de1a64c176361d3cc
Snezana Kragujevac <sneza.kragujevac@gmail.com>
2026-09-15 12:20:44 +0200

    Add recipes route

--- server.ts
+++ server.ts
@@ -10,3 +10,32 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+//recipes
+
+app.get("/recipes", (req, res) => {
+  res.json({
+    title: "My Recipe API"
+  });
+});
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my recipe API!");
+});
+
+app.get("/recipes", (req, res) => {
+  res.json({
+    title: "My Recipe API"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
